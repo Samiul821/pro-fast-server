df4bbd7abc666790ad928185afa64c1be292c7ed
Samiul821 <smsamiul821@gmail.com>
2025-07-07 01:18:59 +0600

    done

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const serviceAccount = require("./firebase-admin-key.json");
+const decodedKey = Buffer.from(process.env.FB_SERVICE_KEY, "base64").toString(
+  "utf8"
+);
+
+const serviceAccount = JSON.parse(decodedKey);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -36,7 +40,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("parcelDB");
     const usersCollection = db.collection("users");
@@ -271,7 +275,7 @@ async function run() {
           $project: {
             status: "$_id",
             count: 1,
-            _id: 0
+            _id: 0,
           },
         },
       ];
@@ -654,10 +658,10 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
